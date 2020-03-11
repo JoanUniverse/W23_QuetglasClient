0d6c483896933403f294f86ebaba25f32d638433
JoanUniverse <jonnyy184@gmail.com>
2020-03-11 12:30:55 +0100

    first commit

---
+++ Assets/Scripts/MainMenu/Helper.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Helper : MonoBehaviour
+{
+
+    internal static IEnumerator InitializeToken(string email, string password)
+    {
+        Player player = FindObjectOfType<Player>();
+        if (string.IsNullOrEmpty(player.Token))
+        {
+            UnityWebRequest httpClient = new UnityWebRequest(player.HttpServerAddress + "Token", "POST");
+
+            // application/x-www-form-urlencoded
+            WWWForm dataToSend = new WWWForm();
+            dataToSend.AddField("grant_type", "password");
+            dataToSend.AddField("username", email);
+            dataToSend.AddField("password", password);
+
+            httpClient.uploadHandler = new UploadHandlerRaw(dataToSend.data);
+            httpClient.downloadHandler = new DownloadHandlerBuffer();
+
+            httpClient.SetRequestHeader("Accept", "application/json");
+            httpClient.certificateHandler = new BypassCertificate();
+            yield return httpClient.SendWebRequest();
+
+            if (httpClient.isNetworkError || httpClient.isHttpError)
+            {
+                throw new Exception("Helper > InitToken: " + httpClient.error);
+            }
+            else
+            {
+                string jsonResponse = httpClient.downloadHandler.text;
+                AuthorizationToken authToken = JsonUtility.FromJson<AuthorizationToken>(jsonResponse);
+                player.Token = authToken.access_token;
+            }
+            httpClient.Dispose();
+        }
+    }
+
+    internal static IEnumerator GetPlayerId()
+    {
+        Player player = FindObjectOfType<Player>();
+        UnityWebRequest httpClient = new UnityWebRequest(player.HttpServerAddress + "api/Account/UserId", "GET");
+
+        httpClient.SetRequestHeader("Authorization", "bearer " + player.Token);
+        httpClient.SetRequestHeader("Accept", "application/json");
+
+        httpClient.downloadHandler = new DownloadHandlerBuffer();
+        httpClient.certificateHandler = new BypassCertificate();
+        yield return httpClient.SendWebRequest();
+
+        if (httpClient.isNetworkError || httpClient.isHttpError)
+        {
+            throw new Exception("Helper > GetPlayerId: " + httpClient.error);
+        }
+        else
+        {
+            string jsonResponse = httpClient.downloadHandler.text;
+            string response = jsonResponse.Replace("\"", "");
+            player.Id = response;
+        }
+
+        httpClient.Dispose();
+    }
+
+    internal static IEnumerator GetPlayerInfo()
+    {
+        Player player = FindObjectOfType<Player>();
+        UnityWebRequest httpClient = new UnityWebRequest(player.HttpServerAddress + "api/Player/Info", "GET");
+
+        httpClient.SetRequestHeader("Authorization", "bearer " + player.Token);
+        httpClient.SetRequestHeader("Accept", "application/json");
+
+        httpClient.downloadHandler = new DownloadHandlerBuffer();
+        httpClient.certificateHandler = new BypassCertificate();
+        yield return httpClient.SendWebRequest();
+
+        if (httpClient.isNetworkError || httpClient.isHttpError)
+        {
+            throw new Exception("Helper > GetPlayerInfo: " + httpClient.error);
+        }
+        else
+        {
+            PlayerSerializable playerSerializable = JsonUtility.FromJson<PlayerSerializable>(httpClient.downloadHandler.text);
+            player.Id = playerSerializable.Id;
+            player.Name = playerSerializable.Name;
+            player.Email = playerSerializable.Email;
+            player.BirthDay = DateTime.Parse(playerSerializable.BirthDay);
+        }
+
+        httpClient.Dispose();
+    }
+
+    internal static IEnumerator NewPlayerOnline()
+    {
+        Player player = FindObjectOfType<Player>();
+        UnityWebRequest httpClient = new UnityWebRequest(player.HttpServerAddress + "api/Player/NewPlayerOnline", "POST");
+
+        string jsonData = JsonUtility.ToJson(player);
+        byte[] dataToSend = Encoding.UTF8.GetBytes(jsonData);
+        httpClient.uploadHandler = new UploadHandlerRaw(dataToSend);
+        httpClient.SetRequestHeader("Content-Type", "application/json");
+        httpClient.SetRequestHeader("Authorization", "bearer " + player.Token);
+
+        httpClient.certificateHandler = new BypassCertificate();
+
+        yield return httpClient.SendWebRequest();
+
+        if (httpClient.isNetworkError || httpClient.isHttpError)
+        {
+            throw new Exception("OnNewPlayerOnline: Error > " + httpClient.error);
+        }
+
+        httpClient.Dispose();
+    }
+    
+}
---
+++ Assets/Scripts/MainMenu/Player.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private const string _httpServerAddress = "https://localhost:44366/";
+    public string HttpServerAddress
+    {
+        get
+        {
+            return _httpServerAddress;
+        } 
+    }
+
+    private string _token;
+    public string Token
+    {
+        get { return _token; }
+        set { _token = value; }
+    }
+
+    private string _id;
+    public string Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+
+    private string _name;
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    private DateTime _birthday;
+    public DateTime BirthDay
+    {
+        get { return _birthday; }
+        set { _birthday = value; }
+    }
+
+    private string _email;
+    public string Email
+    {
+        get { return _email; }
+        set { _email = value; }
+    }
+
+    private void Awake()
+    {
+        var gameManagers = FindObjectsOfType<Player>();
+        if (gameManagers.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+}
